8e325f03b1bcfe69e53d001e711147999b3a791e
Jaiden Ranada <67429387+cinderbag@users.noreply.github.com>
2024-05-29 22:15:51 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,38 @@
+help = """
+/help:      Prints a list of all possible commands
+/define:    Allows user to input values to be used in calculations
+/add:       adds all defined variables
+"""
+
+def commandchecker():
+    x = input()
+    
+    if x == '':
+        print('Blank Command Go ahead and type something!')
+        commandchecker()
+    
+    if x == '/help':
+        print(help)
+        commandchecker()
+    
+    if x == '/graph':
+        definevar()
+        commandchecker()
+
+    else:
+        print('Invalid Command please enter /help for a list of commands')
+        commandchecker()
+
+def definevar():
+    xvalues = []
+    yvalues = []
+    a = input('How many point sets do you have?: ')
+    a = int(a)
+    x = a
+    while x > 0:
+        print('1')
+        x = x-1
+
+
+
+commandchecker()
